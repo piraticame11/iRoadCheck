250da9c1b38912625f6b7042f4bf72fe55db35bf
Salvatorexx <hgcgloria00163@usep.edu.ph>
2025-05-16 01:27:43 +0800

    [VPS] Test export fix

--- app/Livewire/Pages/Staff/RoadDefectReports.php
+++ app/Livewire/Pages/Staff/RoadDefectReports.php
@@ -44,7 +44,7 @@ class RoadDefectReports extends Component
 
     public function mount(Request $request): void
     {
-//        $this->status = $request->query('status'); // 'Unfixed' if coming from the card
+        $this->status = $request->query('status'); // 'Unfixed' if coming from the card
 
         $reportData = Cache::remember('report_dropdown_data', 3600, function () {
             return Report::select('defect', 'status', 'barangay', 'street', 'purok')->get();
@@ -129,9 +129,9 @@ class RoadDefectReports extends Component
             });
         }
 
-//        if ($this->status) {
-//            $query->where('status', $this->status);
-//        }
+        if ($this->status) {
+            $query->where('status', $this->status);
+        }
 
         if ($this->statusFilter) {
             $query->where('status', $this->statusFilter);
@@ -176,10 +176,9 @@ class RoadDefectReports extends Component
 
     public function exportRoadDefectReports(): \Symfony\Component\HttpFoundation\BinaryFileResponse
     {
-        return Excel::download(
-            new RoadDefectReportsExport($this->getFilteredQuery()),
-            'road_defect_reports.xlsx'
-        );
+        $filteredRoadDefectReports = $this->getFilteredQuery()->get();
+
+        return Excel::download(new RoadDefectReportsExport($filteredRoadDefectReports), 'road_defect_reports.xlsx');
     }
 
     public function render(): Factory|View|Application|\Illuminate\View\View
