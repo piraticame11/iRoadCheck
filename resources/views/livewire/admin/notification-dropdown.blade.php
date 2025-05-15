f444f68111c856c1b37810be72669eba63c6dd51
Salvatorexx <hgcgloria00163@usep.edu.ph>
2025-05-16 07:16:09 +0800

    [VPS] notif

--- resources/views/livewire/admin/notification-dropdown.blade.php
+++ resources/views/livewire/admin/notification-dropdown.blade.php
@@ -3,7 +3,7 @@
         dropdownVisible: false,
         notificationCount: @entangle('notifications_count').live,
         showRedDot() {
-
+            return this.notificationCount > 0;
         },
         redirectTo(url) {
             window.location.href = url;
@@ -28,11 +28,13 @@
     </a>
 
     <!-- Notifications Count (Red Dot) -->
-    <span x-cloak
-          x-show="showRedDot()"
-          class="lazyload absolute scale-90 -top-0.5 -right-0.5 grid min-h-[24px] min-w-[24px] max-h-[34px] max-w-[34px] translate-x-2/4 -translate-y-2/4 place-items-center rounded-full border-1 border-[#202020] bg-red-600 py-0.5 px-0.5 text-xs text-white pointer-events-none">
+    @if ($notifications_count > 0)
+        <span
+            class="lazyload absolute scale-90 -top-0.5 -right-0.5 grid min-h-[24px] min-w-[24px] max-h-[34px] max-w-[34px] translate-x-2/4 -translate-y-2/4 place-items-center rounded-full border-1 border-[#202020] bg-red-600 py-0.5 px-0.5 text-xs text-white pointer-events-none">
         {{ $notifications_count }}
     </span>
+    @endif
+
 </div>
 
 {{--    <!-- Dropdown Notifications -->--}}
