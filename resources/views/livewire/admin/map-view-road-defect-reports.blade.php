6a7c358634047955b610143aa44d7ed27b336eeb
piraticame11 <hgcgloria00163@usep.edu.ph>
2025-05-28 03:49:54 +0800

    Changed radius size on map

--- resources/views/livewire/admin/map-view-road-defect-reports.blade.php
+++ resources/views/livewire/admin/map-view-road-defect-reports.blade.php
@@ -1123,7 +1123,7 @@
                         const marker = L.circleMarker([avgLat, avgLng], {
                             color: hasNewReportToday ? 'red' : '#ccc',
                             weight: 1,
-                            radius: 8 + Math.log2(reportCount) * 2,
+                            radius: 16,
                             fillColor: markerColor,
                             fillOpacity: hasNewReportToday ? 0.7 : 0.0
                         }).addTo(this.map);
