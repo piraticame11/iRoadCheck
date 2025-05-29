2990888f8a2a00e1a35e90f933e23250b9adad76
piraticame11 <hgcgloria00163@usep.edu.ph>
2025-05-29 13:03:28 +0800

    DEBUG

--- app/Livewire/ReviewUpdate.php
+++ app/Livewire/ReviewUpdate.php
@@ -106,16 +106,16 @@ use Livewire\Component;
                     usleep(500000); // Wait 0.5 seconds before checking again
                 }
                 // Make sure file exists before attempting to read it
-                if (!file_exists($jsonPath)) {
+                if (file_exists($jsonPath)) {
+                    $jsonData = json_decode(file_get_contents($jsonPath), true);
+                    if (!empty($jsonData['prediction'])) {
+                        return redirect()->back()->with('dont_allow_update_open', true);
+                    } else {
+
+                    }
+                }else{
                     return redirect()->back()->with('ai_is_off', true);
                 }
-
-                $jsonData = json_decode(file_get_contents($jsonPath), true);
-
-                if (!empty($jsonData['prediction'])) {
-                    return redirect()->back()->with('dont_allow_update_open', true);
-                }
-            dd("GOOD NI NA LINE 6");
         }
 
 
