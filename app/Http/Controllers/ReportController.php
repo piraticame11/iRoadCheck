90fb9a95309d7d84460ff27c333ba1b6d35ea63c
piraticame11 <hgcgloria00163@usep.edu.ph>
2025-05-29 11:46:22 +0800

    Fixed algo for staff update

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -422,7 +422,7 @@ class ReportController extends Controller
 
         // Generate filenames
         $timestamp = time();
-        $imageName = "update_photo_{$timestamp}.png";
+//        $imageName = "update_photo_{$timestamp}.png";
 //        $annotatedPath = "update/update_photo_{$timestamp}_annotated.jpg";
         $jsonPath = storage_path("app/public/updates/update_photo_{$timestamp}.json");
         $imageName = "app/public/updates/update_photo_{$timestamp}.json";
